21384e9137e55f64931012cf42d719acfb1d08df
pvshake <pvshakeu@gmail.com>
2022-03-23 13:14:41 -0300

    metade aula 02

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,11 @@
-import { useHistory } from 'react-router-dom';
-
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
-import '../styles/auth.scss';
+import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
-export function Home() {
-  const history = useHistory();
-  
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -23,15 +19,14 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
+          <h2>Criar uma nova sala</h2>
           <form>
-            <input type="text" placeholder="Digite o código da sala" />
-            <Button type="submit">Entrar na sala</Button>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
           </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+          </p>
         </div>
       </main>
     </div>
